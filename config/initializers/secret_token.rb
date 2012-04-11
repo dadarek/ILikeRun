7650be6581a5da575e027d2c35d9dfa7e3ce53bd
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-11 10:26:04 -0500

    Empty rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ILikeRun::Application.config.secret_token = '9312f241a926eb00a0d6dda1584884b1b972eb35ce17cdc745cdc9f7a07e8f88a35b8e28b9dc2cfc3671a3747341f305a98691526b686e6e8b1149d230b2afed'
