e79e951512fa957254bc2dc95746000ee6765615
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-12 11:32:23 -0500

    Converting to capybara.

--- spec/requests/run_log_spec.rb
+++ spec/requests/run_log_spec.rb
@@ -3,15 +3,11 @@ require 'spec_helper'
 describe "index page" do
 
   it "displays success after a log post" do
-    get "/"
+    visit "/"
+    fill_in "time_ran", with: 30
+    click_button "Save"
 
-    assert_select "form.add_run" do
-      assert_select "input[name=?]", "time_ran"
-      assert_select "input[type=?]", "submit"
-    end
-
-    post "/", time_ran: 30
-    assert_select ".message", text: "Success"
-  end
+    page.should have_selector(".message", text: "Success")
+  end 
 
 end
