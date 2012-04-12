c22f82a3c43b2f96101798e63aec29b7374f0946
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-12 09:53:31 -0500

    Some real tests.

--- app/models/run_log.rb
+++ app/models/run_log.rb
@@ -1,3 +1,4 @@
 class RunLog < ActiveRecord::Base
   attr_accessible :time_ran
+  validates :time_ran, numericality: {greater_than_or_equal_to: 1}
 end
--- spec/models/run_log_spec.rb
+++ spec/models/run_log_spec.rb
@@ -2,9 +2,27 @@ require 'spec_helper'
 
 describe RunLog do
 
-  it "Saves time_ran" do
-    run_log = RunLog.create!( time_ran: 30 )
-    run_log.time_ran.should == 30
+  it "Rejects nil run times" do
+    run_log = RunLog.new
+    run_log.should_not be_valid
+  end
+
+  it "Rejects 0 run times" do
+    run_log = RunLog.new
+    run_log.time_ran = 0
+    run_log.should_not be_valid
+  end
+
+  it "Rejects negative run times" do
+    run_log = RunLog.new
+    run_log.time_ran = -30
+    run_log.should_not be_valid
+  end
+
+  it "Accepts positive run times" do
+    run_log = RunLog.new
+    run_log.time_ran = 30
+    run_log.should be_valid
   end
 
 end
