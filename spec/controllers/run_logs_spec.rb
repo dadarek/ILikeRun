55d3ae85803242a30b9d2c855420867c52bf78e7
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-12 13:54:22 -0500

    Refactoring.

--- spec/controllers/run_logs_spec.rb
+++ spec/controllers/run_logs_spec.rb
@@ -3,13 +3,15 @@ require 'spec_helper'
 describe RunLogsController do
 
   describe "#create" do
-    it "should redirect to home page" do
+    before :each do
       post :create, time_ran: 30
+    end
+
+    it "should redirect to home page" do
       response.should redirect_to root_path
     end
 
     it "creates a Runlog" do
-      post :create, time_ran: 30
       RunLog.should have(1).items
     end
 
