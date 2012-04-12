69661608397fb0b15a4b7cd61a580339e33a5235
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-12 15:47:18 -0500

    Removing redirect to home page test.  Not necessary anymore.

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
-describe "first route test" do
-
-  it "routes index to run_logs controller" do
-    { get: "/" }.should route_to "run_logs#index"
-  end
+describe "route tests" do
 
 end
 
