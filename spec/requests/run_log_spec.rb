638f2d739fb90349bc73f79c88141fc721f164ef
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-12 15:41:44 -0500

    Refactoring.

--- spec/requests/run_log_spec.rb
+++ spec/requests/run_log_spec.rb
@@ -10,20 +10,28 @@ describe "index page" do
     fill_in "time_ran", with: 30
     click_button "Save"
 
-    page.should have_selector ".notice"
+    page.should have_notice
   end 
 
   it "does not display success before log post" do
-    page.should_not have_selector ".notice"
+    page.should_not have_notice
   end
 
   it "displays failure after a bad post" do
     click_button "Save"
-    page.should have_selector ".alert"
+    page.should have_alert 
   end
 
   it "does not display failure before post" do
-    page.should_not have_selector ".alert"
+    page.should_not have_alert
+  end
+
+  def have_notice
+    have_selector ".notice"
+  end
+
+  def have_alert
+    have_selector ".alert"
   end
 
 end
