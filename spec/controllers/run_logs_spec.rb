63c08d4d98c030afc027b60e0e713d0cc6f58264
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-12 14:03:35 -0500

    A valid POST context.

--- spec/controllers/run_logs_spec.rb
+++ spec/controllers/run_logs_spec.rb
@@ -3,23 +3,25 @@ require 'spec_helper'
 describe RunLogsController do
 
   describe "#create" do
-    before :each do
-      @time_ran = 30
-      post :create, time_ran: @time_ran
-    end
 
-    it "should redirect to home page" do
-      response.should redirect_to root_path
-    end
+    context "a valid post" do
+      before :each do
+        @time_ran = 30
+        post :create, time_ran: @time_ran
+      end
 
-    it "creates a RunLog with correct time_ran" do
-      RunLog.first.time_ran.should == @time_ran
-    end
+      it "redirects to home page" do
+        response.should redirect_to root_path
+      end
 
-    it "creates a RunLog with correct date" do
-      RunLog.first.created_at.to_date.should == Date.today
+      it "creates a RunLog with correct time_ran" do
+        RunLog.first.time_ran.should == @time_ran
+      end
+
+      it "creates a RunLog with correct date" do
+        RunLog.first.created_at.to_date.should == Date.today
+      end
     end
 
   end
-
 end
