bd42411445b6d855e19f571d39c6408c2ac2dcc4
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-16 17:04:57 -0500

    Adding feature check for link to history from run logs page.

--- features/list_past_runs.feature
+++ features/list_past_runs.feature
@@ -8,3 +8,8 @@ Feature: List past runs
     When I visit the run log history screen
     Then I should see those runs ordered by most-recent first
 
+  Scenario: Navigating to runs
+    Given I am on the enter-run-log page
+    When I click on history
+    Then I go to the history page
+
--- features/step_definitions/list_past_runs_steps.rb
+++ features/step_definitions/list_past_runs_steps.rb
@@ -2,7 +2,19 @@ Given /I visit the run log history screen/ do
   visit '/run_logs'
 end
 
-Given /I have entered some runs in the past/ do
+Given /I am on the enter-run-log page/ do
+  visit '/run_logs/new'
+end
+
+When /I click on history/ do
+  click_link "History"
+end
+
+Then /I go to the history page/ do
+  current_path.should == run_logs_path
+end
+
+When /I have entered some runs in the past/ do
   RunLog.create!( time_ran: 20 )
   RunLog.create!( time_ran: 40 )
   RunLog.create!( time_ran: 30 )
