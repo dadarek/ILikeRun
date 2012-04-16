e64eb900e48a776f37949ab85229ef8ec9adc696
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-16 10:57:52 -0500

    Updating features.

--- features/list_past_runs.feature
+++ features/list_past_runs.feature
@@ -1,12 +1,10 @@
-@wip
 Feature: List past runs
   In order see if I am getting better
   As an obsessive runner
   I want to be able to view my run history
 
   Scenario: Viewing runs
-    Given I am on the home screen
+    Given I am on the run log history screen
     And I have entered some runs in the past
-    When I click on View History
     Then I should see the runs I entered
 
--- features/step_definitions/list_past_runs_steps.rb
+++ features/step_definitions/list_past_runs_steps.rb
@@ -1,11 +1,15 @@
-Given /I am on the home screen/ do
+Given /I am on the run log history screen/ do
+  visit '/run_logs/show'
 end
 
 Given /I have entered some runs in the past/ do
-end
-
-When /I click on View History/ do
+  RunLogs.create!( time_ran: 20 )
+  RunLogs.create!( time_ran: 30 )
+  RunLogs.create!( time_ran: 40 )
 end
 
 Then /I should see the runs I entered/ do
+  page.should contain("20")
+  page.should contain("30")
+  page.should contain("40")
 end
