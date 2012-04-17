0adc83a5c11c70986a376b2a1ad2d8e555d4eb82
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-17 16:53:05 -0500

    Refactoring.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,34 @@ require 'spec_helper'
 
 describe User do
 
+  def params
+    { user_name: "elUser", password: "password", salt: "salt" }
+  end
+
+  def params_without field
+    result = params
+    result.delete field
+    result
+  end
+
   it "rejects empty username" do
-    user = User.create( password: "password", salt: "salt" )
+    user = User.create( params_without :user_name )
     user.should_not be_valid
   end
 
   it "rejects empty password" do
-    user = User.create( user_name: "A", salt: "salt" )
+    user = User.create( params_without :password )
     user.should_not be_valid
   end
 
   it "rejects empty salt" do
-    user = User.create( user_name: "A", password: "P" )
+    user = User.create( params_without :salt )
     user.should_not be_valid
   end
 
   it "requires unique usernames" do
-    user1 = User.create!( user_name: "A", password: "password", salt: "salt" )
-    user2 = User.create( user_name: "A", password: "password", salt: "salt" )
+    user1 = User.create!( params )
+    user2 = User.create( params )
     user1.should be_valid
     user2.should_not be_valid
   end
