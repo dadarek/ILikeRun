d4e8fd6eb363503211ed496b20e58e0ef9562d45
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-17 13:26:20 -0500

    Adding User stubs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120412143302) do
+ActiveRecord::Schema.define(:version => 20120417174902) do
 
   create_table "run_logs", :force => true do |t|
     t.integer  "time_ran"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "user_name"
+    t.string   "password"
+    t.string   "salt"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
