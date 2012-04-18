585302a8af2f50ace4c848c22465e1ef35cbbea0
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-18 17:35:50 -0500

    Refactoring.

--- spec/views/layout_spec.rb
+++ spec/views/layout_spec.rb
@@ -2,45 +2,50 @@ require 'spec_helper'
 
 describe "layouts/application" do
 
-  def logout_selector
-    "a[href='#{logout_path}']"
+  def have_logout 
+    have_selector "a[href='#{logout_path}']"
   end
 
-  def welcome_selector
-    ".welcome-text"
+  def have_welcome
+    have_selector ".welcome-text"
   end
+
+  def have_notice
+    have_selector ".notice" 
+  end
+
 
   it "does not render welcome text if user not present" do
     render
-    rendered.should_not have_selector welcome_selector
+    rendered.should_not have_welcome
   end
   
   it "renders welcome text if user present" do
     session[:user_id] = 5
     render
-    rendered.should have_selector welcome_selector
+    rendered.should have_welcome
   end
 
   it "renders logout link if user present" do
     session[:user_id] = 5
     render
-    rendered.should have_selector logout_selector
+    rendered.should have_logout
   end
 
   it "does not render logout link if user not present" do
     render
-    rendered.should_not have_selector logout_selector
+    rendered.should_not have_logout
   end
 
   it "renders notice if flash[:notice] present" do
     flash[:notice] = "Some notice"
     render
-    rendered.should have_selector ".notice" 
+    rendered.should have_notice
   end
 
   it "does not render notice if flash[:notice] is missing" do
     render
-    rendered.should_not have_selector ".notice" 
+    rendered.should_not have_notice
   end
 
   pending "How do you print out 'Welcome, username?"
