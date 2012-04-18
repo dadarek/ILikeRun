8b68c3056bc764f10c2fb23687deda27d13ae3d8
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-18 15:28:01 -0500

    Refactoring.

--- spec/views/layout_spec.rb
+++ spec/views/layout_spec.rb
@@ -2,28 +2,34 @@ require 'spec_helper'
 
 describe "layouts/application" do
 
+  def logout_selector
+    "a[href='#{logout_path}']"
+  end
+
+  def welcome_selector
+    ".welcome-text"
+  end
+
   it "does not render welcome text if user not present" do
     render
-    rendered.should_not have_selector ".welcome-text"
+    rendered.should_not have_selector welcome_selector
   end
   
   it "renders welcome text if user present" do
     session[:user_id] = 5
     render
-    rendered.should have_selector ".welcome-text"
+    rendered.should have_selector welcome_selector
   end
 
   it "renders logout link if user present" do
     session[:user_id] = 5
     render
-    selector = "a[href='#{logout_path}']"
-    rendered.should have_selector(selector)
+    rendered.should have_selector logout_selector
   end
 
   it "does not render logout link if user not present" do
     render
-    selector = "a[href='#{logout_path}']"
-    rendered.should_not have_selector(selector)
+    rendered.should_not have_selector logout_selector
   end
 
 end
