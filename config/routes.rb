3581083111d337ffd22fc7ad6d89c60d94d0cf4a
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-18 13:42:47 -0500

    Adding route name.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ ILikeRun::Application.routes.draw do
   resources :run_logs, only: [:create, :new, :index]
 
   match '/users/login' => 'users#login', :via => :get
-  match '/users/login' => 'users#authenticate', :via => :post
+  match '/users/login' => 'users#authenticate', :via => :post, :as => "authenticate"
   root :to => 'users#login'
 
   # Sample of regular route:
