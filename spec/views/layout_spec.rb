b10739b1b8571da9cd6217186428b7720450748d
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-18 15:25:38 -0500

    Making sure logout link is only displayed when user is present.

--- spec/views/layout_spec.rb
+++ spec/views/layout_spec.rb
@@ -2,15 +2,28 @@ require 'spec_helper'
 
 describe "layouts/application" do
 
-  it "should not render welcome text if user present" do
+  it "does not render welcome text if user not present" do
     render
     rendered.should_not have_selector ".welcome-text"
   end
   
-  it "should render welcome text if user present" do
+  it "renders welcome text if user present" do
     session[:user_id] = 5
     render
     rendered.should have_selector ".welcome-text"
+  end
+
+  it "renders logout link if user present" do
+    session[:user_id] = 5
+    render
+    selector = "a[href='#{logout_path}']"
+    rendered.should have_selector(selector)
+  end
+
+  it "does not render logout link if user not present" do
+    render
+    selector = "a[href='#{logout_path}']"
+    rendered.should_not have_selector(selector)
   end
 
 end
