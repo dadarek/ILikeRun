d60101d53d46f57b1e026f62deacfcdb0cfd02a0
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-18 09:27:42 -0500

    Refactoring.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,26 @@ require 'spec_helper'
 
 describe User do
 
-  def params
-    { user_name: "elUser", password: "password" }
+  def create_user 
+    User.create(user_name: "user", password: "password")
   end
 
-  def params_without field
-    result = params
-    result.delete field
-    result
+  def params 
+    {user_name: "user", password: "password"}
   end
 
   it "rejects empty username" do
-    user = User.create( params_without :user_name )
+    user = User.create( password: "passw" )
     user.should_not be_valid
   end
 
   it "rejects empty password" do
-    user = User.create( params_without :password )
+    user = User.create( user_name: "UserName" )
     user.should_not be_valid
   end
 
   it "creates salt on save" do
-    user = User.create!( params_without :salt )
+    user = create_user
     user.salt.should_not be_nil
   end
 
@@ -32,25 +30,25 @@ describe User do
   end
 
   it "requires unique usernames" do
-    user1 = User.create!(params)
-    user2 = User.create(params)
+    user1 = create_user
+    user2 = create_user
     user1.should be_valid
     user2.should_not be_valid
   end
 
   it "encrypts password before saving" do
-    user = User.create!(params)
+    user = create_user
     encrypted = BCrypt::Engine.hash_secret params[:password], user.salt
     user.password.should == encrypted
   end
 
   it "returns a valid user on succesful authentication" do
-    user = User.create!(params)
+    user = create_user
     User.authenticate( params[:user_name], params[:password] ).should == user
   end
 
   it "returns nil on bad password" do
-    user = User.create!(params)
+    user = create_user
     User.authenticate( params[:user_name], "bad-password" ).should be_nil
   end
 
