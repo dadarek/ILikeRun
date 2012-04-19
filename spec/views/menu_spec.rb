927c84d4e6f2564f88a889a89efa004be9ad980b
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-19 11:16:16 -0500

    Making sure links are only displayed when you're logged in.

--- spec/views/menu_spec.rb
+++ spec/views/menu_spec.rb
@@ -6,15 +6,27 @@ describe "layouts/_menu" do
     have_selector "a[href='#{logout_path}']"
   end
 
-  it "renders logout link if user present" do
+  def have_history
+    have_selector "a[href='#{run_logs_path}']"
+  end
+
+  def have_schedule
+    have_selector "a[href='#']"
+  end
+
+  it "renders menu links if user present" do
     session[:user_id] = 5
     render
     rendered.should have_logout
+    rendered.should have_history
+    rendered.should have_schedule
   end
 
-  it "does not render logout link if user not present" do
+  it "does not render menu links if user not present" do
     render
     rendered.should_not have_logout
+    rendered.should_not have_history
+    rendered.should_not have_schedule
   end
 
 end
