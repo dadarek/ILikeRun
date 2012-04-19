aa99defd4f2cd4a635e85f2df036b7045b6c02a4
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-19 11:05:42 -0500

    Moving alert and notice messages into partial view.

--- spec/views/layout_spec.rb
+++ spec/views/layout_spec.rb
@@ -10,15 +10,6 @@ describe "layouts/application" do
     have_selector ".welcome-text"
   end
 
-  def have_notice
-    have_selector ".notice" 
-  end
-
-  def have_alert
-    have_selector ".alert" 
-  end
-
-
   it "does not render welcome text if user not present" do
     render
     rendered.should_not have_welcome
@@ -39,30 +30,6 @@ describe "layouts/application" do
   it "does not render logout link if user not present" do
     render
     rendered.should_not have_logout
-  end
-
-  it "renders notice if flash[:notice] present" do
-    flash[:notice] = "el notice"
-    render
-    rendered.should have_notice
-    rendered.should =~ /el notice/
-  end
-
-  it "does not render notice if flash[:notice] is missing" do
-    render
-    rendered.should_not have_notice
-  end
-
-  it "renders alert if flash[:alert] present" do
-    flash[:alert] = "Some alert"
-    render
-    rendered.should have_alert
-    rendered.should =~ /Some alert/
-  end
-
-  it "renders alert if flash[:alert] is missing" do
-    render
-    rendered.should_not have_alert
   end
 
   pending "Should we check content of .alert and .notice?"
---
+++ spec/views/messages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "layouts/_messages" do
+
+  def have_notice
+    have_selector ".notice" 
+  end
+
+  def have_alert
+    have_selector ".alert" 
+  end
+
+  it "compiles" do
+    flash[:notice] = "el notice"
+    render
+    rendered.should =~ /el notice/
+  end
+
+  it "renders notice if flash[:notice] present" do
+    flash[:notice] = "el notice"
+    render
+    rendered.should have_notice
+    rendered.should =~ /el notice/
+  end
+
+  it "does not render notice if flash[:notice] is missing" do
+    render
+    rendered.should_not have_notice
+  end
+
+  it "renders alert if flash[:alert] present" do
+    flash[:alert] = "Some alert"
+    render
+    rendered.should have_alert
+    rendered.should =~ /Some alert/
+  end
+
+  it "renders alert if flash[:alert] is missing" do
+    render
+    rendered.should_not have_alert
+  end
+
+end
