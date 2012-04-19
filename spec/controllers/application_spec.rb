fabc73d7da1c6bc3c9268d0ef11a9d6a4bc2afc3
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-19 10:23:34 -0500

    Refactoring.

--- spec/controllers/application_spec.rb
+++ spec/controllers/application_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe ApplicationController do
 
+  before :each do
+    @controller = ApplicationController.new
+    @controller.stub(:session).and_return(session)
+  end
+
   it "has an authenticate before filter" do
     all_filters = ApplicationController._process_action_callbacks
     before_filters = all_filters.select{|f| f.kind == :before }
@@ -16,20 +21,11 @@ describe ApplicationController do
     #c.ensure_is_logged_in.should redirect_to(:users_login)
   end
 
-
-  def stub_session
-    @controller = ApplicationController.new
-    @controller.stub(:session).and_return(session)
-  end
-
   it "returns no user if session[:user_id] is nil" do
-    stub_session
     @controller.current_user.should be_nil
   end
 
   it "returns user associated with session[:user_id]" do
-    stub_session
-
     user = User.create!( user_name: "bob", password: "barker" )
     session[:user_id] = user.id
 
