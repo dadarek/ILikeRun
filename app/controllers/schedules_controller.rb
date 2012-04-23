92af58cc4efe68271ee4eb2984c0fda239ba9d42
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-23 15:26:02 -0500

    Renaming

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -8,17 +8,17 @@ class SchedulesController < ApplicationController
   end
 
   def destroy
-    set_day false
+    set_day_of_week false
     render nothing: true
   end
 
   def create
-    set_day true
+    set_day_of_week true
     render nothing: true
   end
 
   private 
-  def set_day value
+  def set_day_of_week value
     day_of_week = params[:day_of_week]
     day_of_week.downcase!
     day_of_week << "="
