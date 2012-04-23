63a0a6d29d1f97ae26f2d63c5a252d3b009702a8
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-23 12:43:55 -0500

    Appropriate image should be displayed.

--- spec/views/schedules.rb
+++ spec/views/schedules.rb
@@ -3,10 +3,23 @@ require 'spec_helper'
 describe "schedules/index" do
 
   it "contains all days of week" do
+    assign :schedule, Schedule.new
     render
     Date::DAYNAMES.each{ |day|
       rendered.should =~ Regexp.new(day)
     }
+  end
+
+  it "displays yes and no images" do
+    schedule = Schedule.new
+    schedule.monday = true
+    schedule.saturday  = true
+    schedule.sunday = true
+
+    assign :schedule, schedule
+    render
+    rendered.should have_selector "img[src$='not-running.jpg']", count: 4
+    rendered.should have_selector "img[src$='yes-running.jpg']", count: 3
   end
 
 end
