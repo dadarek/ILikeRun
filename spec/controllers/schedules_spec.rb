53c235ad9c49b69451a9272678326f4623017a9f
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-23 15:06:49 -0500

    Refactoring.

--- spec/controllers/schedules_spec.rb
+++ spec/controllers/schedules_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe SchedulesController do
 
+  before :each do
+    @user = create_user
+    login @user
+  end
+
   describe "#index" do
 
     def create_schedule user
@@ -9,28 +14,22 @@ describe SchedulesController do
     end
 
     it "returns users schedule" do
-      user = create_user
-      create_schedule user
-
-      login user
+      create_schedule @user
       get :index
-      assigns(:schedule).should == user.schedule
+      assigns(:schedule).should == @user.schedule
     end
 
     it "does not return others schedule" do
-      u1 = create_user("a", "p")
       u2 = create_user("b", "q")
 
-      create_schedule u1
       create_schedule u2
+      create_schedule @user
 
-      login u2
       get :index
-      assigns(:schedule).should == u2.schedule
+      assigns(:schedule).should == @user.schedule
     end
 
     it "assigns blank schedule if none exists" do
-      login(create_user)
       get :index
       assigns(:schedule).should be_instance_of(Schedule)
     end
@@ -41,9 +40,7 @@ describe SchedulesController do
   describe "#delete" do
     
     it "sets yes-running days to not-running days" do
-      user = create_user
-      login user
-      schedule = Schedule.create!(user_id: user.id)
+      schedule = Schedule.create!(user_id: @user.id)
       schedule.monday = true
 
       delete :destroy, day_of_week: "Monday"
