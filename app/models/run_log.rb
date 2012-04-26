02ccd5279de1e08f3126b854c5409c9de400dd38
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-26 11:05:02 -0500

    Adding checks for created_at.

--- app/models/run_log.rb
+++ app/models/run_log.rb
@@ -1,4 +1,5 @@
 class RunLog < ActiveRecord::Base
   attr_accessible :time_ran, :user_id, :created_at
   validates :time_ran, numericality: {greater_than_or_equal_to: 1}
+  validates :created_at, presence: true
 end
--- spec/models/run_log_spec.rb
+++ spec/models/run_log_spec.rb
@@ -4,30 +4,37 @@ describe RunLog do
 
   it "Rejects nil run times" do
     run_log = RunLog.new
+    run_log.created_at = Date.today
+    run_log.time_ran = nil
     run_log.should_not be_valid
   end
 
   it "Rejects 0 run times" do
     run_log = RunLog.new
+    run_log.created_at = Date.today
     run_log.time_ran = 0
     run_log.should_not be_valid
   end
 
   it "Rejects negative run times" do
     run_log = RunLog.new
+    run_log.created_at = Date.today
     run_log.time_ran = -30
     run_log.should_not be_valid
   end
 
   it "Accepts positive run times" do
     run_log = RunLog.new
+    run_log.created_at = Date.today
     run_log.time_ran = 30
     run_log.should be_valid
   end
 
-  it "Creates run log with today's date" do
-    run_log = RunLog.create!( time_ran: 22, user_id: 5)
-    run_log.created_at.to_date.should == Date.today
+  it "Rejects null dates" do
+    run_log = RunLog.new
+    run_log.time_ran = 30
+    run_log.created_at = nil
+    run_log.should_not be_valid
   end
 
 end
