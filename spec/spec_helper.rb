f428caf159960d0bb0f202eb3ebab7ff5b17593d
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-26 18:06:08 -0500

    Adding JASMINE_RAKE ev to hide warnings that jasmine complains about.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["USE_JASMINE_RAKE"] = "true"
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
