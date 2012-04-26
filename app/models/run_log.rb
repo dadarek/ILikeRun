4618fe2f43b460f8422e0d8133625f57ffdfa0df
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-26 14:30:04 -0500

    Temporarily removing validation so that heroku can migrate app.

--- app/models/run_log.rb
+++ app/models/run_log.rb
@@ -1,5 +1,5 @@
 class RunLog < ActiveRecord::Base
   attr_accessible :time_ran, :user_id, :date_ran
   validates :time_ran, numericality: {greater_than_or_equal_to: 1}
-  validates_inclusion_of :date_ran, in: Date.civil(2000, 1, 1)..Date.today, message: "must be between January 1, 2000 and today."
+ # validates_inclusion_of :date_ran, in: Date.civil(2000, 1, 1)..Date.today, message: "must be between January 1, 2000 and today."
 end
