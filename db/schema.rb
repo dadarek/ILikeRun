1d9dc7d269340d91ee8ff41a1698714b754aa066
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-26 11:15:31 -0500

    Adding migration for date ran field.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423160453) do
+ActiveRecord::Schema.define(:version => 20120426161354) do
 
   create_table "run_logs", :force => true do |t|
     t.integer  "time_ran"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id",    :null => false
+    t.date     "date_ran"
   end
 
   create_table "schedules", :force => true do |t|
