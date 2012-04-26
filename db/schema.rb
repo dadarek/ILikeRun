df250b4b2809f5842b159b63b75daa6173d479ae
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-26 11:33:39 -0500

    Updated schema after last migration (storing the default run_date.)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120426161354) do
+ActiveRecord::Schema.define(:version => 20120426162529) do
 
   create_table "run_logs", :force => true do |t|
     t.integer  "time_ran"
