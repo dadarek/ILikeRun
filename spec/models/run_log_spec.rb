05ea1fd5629d78ccac5b6f865996a2a8f5114942
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-26 11:06:25 -0500

    Refactoring.

--- spec/models/run_log_spec.rb
+++ spec/models/run_log_spec.rb
@@ -2,39 +2,34 @@ require 'spec_helper'
 
 describe RunLog do
 
+  before :each do
+    @run_log = RunLog.new
+    @run_log.created_at = Date.today
+    @run_log.time_ran = 30
+  end
+
   it "Rejects nil run times" do
-    run_log = RunLog.new
-    run_log.created_at = Date.today
-    run_log.time_ran = nil
-    run_log.should_not be_valid
+    @run_log.time_ran = nil
+    @run_log.should_not be_valid
   end
 
   it "Rejects 0 run times" do
-    run_log = RunLog.new
-    run_log.created_at = Date.today
-    run_log.time_ran = 0
-    run_log.should_not be_valid
+    @run_log.time_ran = 0
+    @run_log.should_not be_valid
   end
 
   it "Rejects negative run times" do
-    run_log = RunLog.new
-    run_log.created_at = Date.today
-    run_log.time_ran = -30
-    run_log.should_not be_valid
+    @run_log.time_ran = -30
+    @run_log.should_not be_valid
   end
 
   it "Accepts positive run times" do
-    run_log = RunLog.new
-    run_log.created_at = Date.today
-    run_log.time_ran = 30
-    run_log.should be_valid
+    @run_log.should be_valid
   end
 
   it "Rejects null dates" do
-    run_log = RunLog.new
-    run_log.time_ran = 30
-    run_log.created_at = nil
-    run_log.should_not be_valid
+    @run_log.created_at = nil
+    @run_log.should_not be_valid
   end
 
 end
