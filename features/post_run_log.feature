c06c50f0e145c07c8ae082ccc3ea37f0c116357a
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-27 09:52:03 -0500

    Adding features for adding a date ran.

--- features/post_run_log.feature
+++ features/post_run_log.feature
@@ -22,3 +22,11 @@ Feature: Posting a new run log
     And I visit the Run log screen 
     Then I should not see a success message
     And I should not see an error message
+
+  Scenario: Posting a past run
+    Given I am logged in
+    And I visit the Run log screen 
+    When I enter 560 minutes for my time ran
+    And I enter "2005-05-13" for my date ran
+    And I click the save button
+    Then I should have a run log with date "2005-05-13" and time ran "560"
--- features/step_definitions/run_log_steps.rb
+++ features/step_definitions/run_log_steps.rb
@@ -6,6 +6,10 @@ When /I enter (\-?\d+) minutes for my time ran/ do |minutes|
   fill_in "run_log[time_ran]", with: minutes
 end
 
+When /I enter "(.+)" for my date ran/ do |date|
+  fill_in "run_log[date_ran]", with: date
+end
+
 When /I click the save button/ do
   click_button "Save"
 end
@@ -24,5 +28,11 @@ end
 
 Then /I should see an error message/ do
   page.should have_selector ".alert"
+end
+
+Then /I should have a run log with date "([^"]+)" and time ran "([^"]+)"/ do |date_ran, time_ran|
+  log =  @user.run_logs.first
+  log.time_ran.should == Integer(time_ran)
+  log.date_ran.should == Date.parse(date_ran)
 end
 
