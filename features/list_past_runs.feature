6b492f462095518b249921ddcb15603e9a93066d
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-30 12:45:52 -0500

    Getting rid of unecessary feature test.

--- features/list_past_runs.feature
+++ features/list_past_runs.feature
@@ -23,20 +23,6 @@ Feature: List past runs
       | 2000-03-21 | 88 |
       | 2001-11-22 | 99 |
 
-  Scenario: Viewing runs in order
-    Given I am logged in
-    And I have the following run logs
-      | date_ran | time_ran |
-      | 2008-10-12 | 15 |
-      | 2012-02-12 | 20 |
-      | 2002-03-30 | 10 |
-    When I visit the run log history screen
-    Then I should see the following run logs in this order
-      | date_ran | time_ran |
-      | 2012-02-12 | 20 |
-      | 2008-10-12 | 15 |
-      | 2002-03-30 | 10 |
-
   Scenario: Navigating to runs
     Given I am logged in
     And I visit the enter-run-log screen 
--- features/step_definitions/list_past_runs_steps.rb
+++ features/step_definitions/list_past_runs_steps.rb
@@ -16,29 +16,24 @@ end
 
 Then /^I should not see the following run logs$/ do |table|
   table.hashes.each do |hash|
-    time_ran = Regexp.new hash[:time_ran]
-    date_ran = Regexp.new hash[:date_ran]
-    page.body.should_not have_selector("td", text: time_ran)
-    page.body.should_not have_selector("td", text: date_ran)
+    page.body.should_not have_run_log_date hash
+    page.body.should_not have_run_log_time hash
   end
 end 
 
 Then /^I should see the following run logs$/ do |table|
   table.hashes.each do |hash|
-    time_ran = Regexp.new hash[:time_ran]
-    date_ran = Regexp.new hash[:date_ran]
-    page.body.should have_selector("td", text: time_ran)
-    page.body.should have_selector("td", text: date_ran)
+    page.body.should have_run_log_date hash
+    page.body.should have_run_log_time hash
   end
 end 
 
-Then /^I should see the following run logs in this order$/ do |table|
-  dates = table.hashes.collect{ |hash| hash[:date_ran] }
-  times = table.hashes.collect{ |hash| hash[:time_ran] }
-  dates_r = Regexp.new(dates.join(".*?"), Regexp::MULTILINE)
-  times_r = Regexp.new(times.join(".*?"), Regexp::MULTILINE)
+def have_run_log_time hash
+  time_ran = Regexp.new hash[:time_ran]
+  have_selector("td", text: time_ran)
+end
 
-  page.body.should =~ dates_r
-  page.body.should =~ times_r
-end 
-
+def have_run_log_date hash
+  date_ran = Regexp.new hash[:date_ran]
+  have_selector("td", text: date_ran)
+end
