bcc1bacc313aa8a2c897f23a4220a2fba794f4a3
Dariusz Pasciak <dariusz@8thlight.com>
2012-04-30 16:32:11 -0500

    Refactoring.

--- spec/controllers/api_spec.rb
+++ spec/controllers/api_spec.rb
@@ -2,27 +2,41 @@ require 'spec_helper'
 
 describe ApiController do
 
-  it "allows unauthenticated users" do
-    (get :get_user_logs).should_not be_redirect
+  context "Miscellaneous" do
+
+    it "allows unauthenticated users" do
+      (get :get_user_logs).should_not be_redirect
+    end
+
+    it "sends back empty json on invalid user" do
+      get :get_user_logs
+      response.body.should == [].to_json
+    end
+
   end
 
-  it "sends back empty json on invalid user" do
-    get :get_user_logs
-    response.body.should == [].to_json
-  end
+  context "Valid requests" do
 
-  it "sends back given users logs" do
-    user = create_user("Bob")
-    RunLog.create!(time_ran: 50, date_ran: Date.today, user_id: user.id)
-    get :get_user_logs, user_name: "Bob"
-    response.body.should == user.run_logs.to_json( only: [:date_ran, :time_ran])
-  end
+    before :each do
+      user = create_user("Bob")
+      log1 = RunLog.create!(time_ran: 50, date_ran: Date.today, user_id: user.id)
+      log2 = RunLog.create!(time_ran: 24, date_ran: Date.today - 3.days, user_id: user.id)
+      @logs = [log1, log2]
+    end
+    
+    it "sends back given users logs" do
+      get :get_user_logs, user_name: "Bob"
+      response.body.should == logs_to_json
+    end
 
-  it "ignores case" do
-    user = create_user("Bob")
-    RunLog.create!(time_ran: 50, date_ran: Date.today, user_id: user.id)
-    get :get_user_logs, user_name: "bob"
-    response.body.should == user.run_logs.to_json( only: [:date_ran, :time_ran])
+    it "ignores case" do
+      get :get_user_logs, user_name: "bob"
+      response.body.should == logs_to_json
+    end
+
+    def logs_to_json
+      @logs.to_json( only: [:date_ran, :time_ran])
+    end
   end
 
 end
