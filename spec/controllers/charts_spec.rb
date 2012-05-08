43e95141efe3e236d06c53905cb646cbf4c8b3e3
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-08 14:32:26 -0500

    Refactoring.

--- spec/controllers/charts_spec.rb
+++ spec/controllers/charts_spec.rb
@@ -7,42 +7,44 @@ describe ChartsController do
     login @user
   end
 
-  it "ignores logs older than 2 weeks" do
-    log1 = create_log Date.today
-    log2 = create_log Date.today - 5.days
-    log3 = create_log Date.today - 1.week
+  context 'index' do
+    it "ignores logs older than 2 weeks" do
+      log1 = create_log Date.today
+      log2 = create_log Date.today - 5.days
+      log3 = create_log Date.today - 1.week
 
-    thirteen_days_ago = create_log Date.today - 13.days
-    two_weeks_ago = create_log Date.today - 2.weeks
+      thirteen_days_ago = create_log Date.today - 13.days
+      two_weeks_ago = create_log Date.today - 2.weeks
 
-    logs_url = create_logs_url [log1, log2, log3, thirteen_days_ago]
+      logs_url = create_logs_url [log1, log2, log3, thirteen_days_ago]
 
-    HTTParty.should_receive(:get).with(logs_url)
-    get :index
-  end
+      HTTParty.should_receive(:get).with(logs_url)
+      get :index
+    end
 
-  it 'redirects if error connecting to charting server' do
-    HTTParty.stub(:get).and_throw(:exception)
-    (get :index).should redirect_to(action: "unavailable")
-  end
+    it 'redirects if error connecting to charting server' do
+      HTTParty.stub(:get).and_throw(:exception)
+      (get :index).should redirect_to(action: "unavailable")
+    end
 
-  it 'assigns chart_data_points to return value of url' do
-    HTTParty.stub(:get).and_return('meow meow meow')
-    get :index
-    assigns(:chart_data_points).should == 'meow meow meow'
-  end
+    it 'assigns chart_data_points to return value of url' do
+      HTTParty.stub(:get).and_return('meow meow meow')
+      get :index
+      assigns(:chart_data_points).should == 'meow meow meow'
+    end
 
-  def create_logs_url logs
-    logs_json = logs.to_json( only: [:date_ran, :time_ran] )
-    logs_encoded = URI.encode logs_json
+    def create_logs_url logs
+      logs_json = logs.to_json( only: [:date_ran, :time_ran] )
+      logs_encoded = URI.encode logs_json
 
-    charting_server = Rails.configuration.charting_server
+      charting_server = Rails.configuration.charting_server
 
-    charting_server + "/?" + logs_encoded
-  end
+      charting_server + "/?" + logs_encoded
+    end
 
-  def create_log date_ran
-    RunLog.create!(date_ran: date_ran, time_ran: 1, user_id: @user.id)
+    def create_log date_ran
+      RunLog.create!(date_ran: date_ran, time_ran: 1, user_id: @user.id)
+    end
   end
 
 end
