7fbb7dc8eafb2e397a0c0d904c1aa95f87162eba
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-08 15:07:50 -0500

    Refactoring.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,23 +1,31 @@
 class ChartsController < ApplicationController
 
   def index
-    user_logs = current_user.run_logs
-    two_week_logs = user_logs.where('date_ran > ?', Date.today - 2.weeks)
-    logs_json = two_week_logs.to_json( only: [:date_ran, :time_ran] )
-    json_encoded = URI.encode logs_json
-
-    server = Rails.configuration.charting_server
-
-    logs_data_url = "#{server}/?#{json_encoded}"
-
     begin
-      @chart_data_points = HTTParty.get logs_data_url
+      @chart_data_points = get_chart_data_points
     rescue
       flash[:alert] = 'An error occured while contacting the charting server.'
       redirect_to action: "unavailable"
     else
       render :index
     end
+  end
+
+  private
+
+  def get_chart_data_points
+    server = Rails.configuration.charting_server
+    logs_data_url = "#{server}/?#{uri_encoded_json_logs}"
+    HTTParty.get logs_data_url
+  end
+
+  def uri_encoded_json_logs
+    logs_json = user_logs_from_last_2_weeks.to_json( only: [:date_ran, :time_ran] )
+    URI.encode logs_json
+  end
+
+  def user_logs_from_last_2_weeks
+    current_user.run_logs.where('date_ran > ?', Date.today - 2.weeks)
   end
 
 end
